9482036d0de7973168c72c7420c0ae9c3eb4e101
gksmf0324 <gksmfdl0324@naver.com>
2021-12-21 21:59:25 +0900

    12/21

---
+++ Unity_Basic/Unity_CSharp_For/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Unity_CSharp_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arr_PersonName = new string[6];
+
+            arr_PersonName[0] = "김아무개";
+            arr_PersonName[1] = "강아무개";
+            arr_PersonName[2] = "감아무개";
+            arr_PersonName[3] = "이아무개";
+            arr_PersonName[4] = "임아무개";
+            arr_PersonName[5] = "암아무개";
+            int length = arr_PersonName.Length;
+            // for문의 형태
+            // for( 인덱스용 변수 선언 및 초기화 ; for문을 실행할 조건 ; 루프가 한번 실행 될 때마다 실행할 구문)
+
+            for (int i = 0; i < length; i++)
+            {
+                if (arr_PersonName[i] == "강아무개")
+                    Console.WriteLine(arr_PersonName[i]);
+            }
+
+            //2n(n은 양의 정수) 마다 김아무개 라는 규칙을 활용한 예제
+            for (int i= 0; i < length; i+=2)
+            {
+                Console.WriteLine(arr_PersonName[i]);
+            }
+
+        }
+    }
+}
---
+++ Unity_Basic/Unity_CSharp_ForLoopExample/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Unity_CSharp_ForLoopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Orc[] orc_arr = new Orc[10];
+
+            for(int i = 0;i<10;i++)
+            {
+                orc_arr[i] = new Orc();
+                orc_arr[i].name = $"오크{i}";
+            }
+
+            //for (int i = 0; i < 10; i += 2)
+            //{
+            //    orc_arr[i].isResting = true;
+            //}
+            //for(int i = 1; i < 10; i += 2)
+            //{
+            //    orc_arr[i].isResting = false;
+            //}
+           
+            for(int i = 0;i<10; i++)
+            {
+                orc_arr[i].isResting = GetRandomBool();
+            }
+            for(int i = 0; i<10;i++)
+            {
+                if (orc_arr[i].isResting == true)
+                    orc_arr[i].Jump();
+            }
+        }
+
+        static public bool GetRandomBool()
+        {
+            Random rand = new Random();
+            bool value = Convert.ToBoolean(rand.Next(0, 2));
+            return value;
+        }
+    }
+
+    public class Orc
+
+    {
+        public string name;
+        public float height;
+        public float weight;
+        public int age;
+        public char genderChar;
+        public bool isResting;
+
+        public void Smash()
+
+        {
+             Console.WriteLine($"{name} (이)가 휘둘렀다!");
+        }
+
+        public void Jump()
+
+        {
+            Console.WriteLine($"{name} (이)가 점프했다!");
+        }
+
+        public void Dash()
+
+        {
+            Console.WriteLine($"{name} (이)가 돌진했다");
+        }
+        public void CheckIsRestingAndDash()
+
+        {
+            if (isResting)
+            {
+                Dash();
+            }
+
+            else
+            {
+                Console.WriteLine($"{name} (이)는 바빠서 돌진을 할 수가 없다");
+            }
+        }
+
+    }
+
+} 
+
